a21e086316a58c5751b3d50c23f7728b19d4d400
Cenk <muzzyathome@gmail.com>
2022-03-12 15:15:46 +0000

    Plateau definition max values for x and y added

--- src/mars-plateau.ts
+++ src/mars-plateau.ts
@@ -1,8 +1,11 @@
+const  MAX_X:number  = 5;
+const  MAX_y:number  = 5;
+
 export class Plateau {
         x: number;
         y: number;
         constructor(x: number, y: number) {
-            this.x = x;
-            this.y = y;   
+            this.x = (x <= MAX_X) && (x > 0) ? x : MAX_X;
+            this.y = (y <= MAX_y) && (y > 0) ? y : MAX_y;   
         }
 }
--- test/mars-plateau.test.ts
+++ test/mars-plateau.test.ts
@@ -3,12 +3,14 @@ import * as MARS from "../src/mars-plateau";
 describe('Set up plateau', () => {
         test.each`
             x | y | expected
-            ${7} | ${9}	| ${{"x": 7, "y": 9}}
-            ${2} | ${2}	| ${{"x": 2, "y": 2}}
-            ${2} | ${2}	| ${{"x": 2, "y": 2}}
-            ${100} | ${100}	| ${{"x": 100, "y": 100}}
+            ${7} | ${9}	| ${{"x": 5, "y": 5}}
+            ${-2} | ${2}	| ${{"x": 5, "y": 2}}
+            ${2} | ${-2}	| ${{"x": 2, "y": 5}}
+            ${100} | ${100}	| ${{"x": 5, "y": 5}}
+            ${3} | ${3}	| ${{"x": 3, "y": 3}}
         `('Plateau($x,$y) = $expected', ({ x,y, expected}) => {
             let plateau: MARS.Plateau  = new MARS.Plateau(x,y);
             expect(plateau).toEqual(expected);
         });
 });
+
