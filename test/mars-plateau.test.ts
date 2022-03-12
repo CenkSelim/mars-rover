2cddd8797bf40abb2413ad76aedaf21e8ee19371
Cenk <muzzyathome@gmail.com>
2022-03-12 14:55:30 +0000

    Plateau definition corrected spelling

--- test/mars-plateau.test.ts
+++ test/mars-plateau.test.ts
@@ -2,13 +2,13 @@ import * as MARS from "../src/mars-plateau";
 // 1. test Plateau creation
 describe('Set up plateau', () => {
         test.each`
-            x | y | expexted
+            x | y | expected
             ${7} | ${9}	| ${{"x": 7, "y": 9}}
             ${2} | ${2}	| ${{"x": 2, "y": 2}}
             ${2} | ${2}	| ${{"x": 2, "y": 2}}
             ${100} | ${100}	| ${{"x": 100, "y": 100}}
-        `('Plateau($x,$y) = $upper-right-x, upper-right-y', ({ x,y, expexted}) => {
+        `('Plateau($x,$y) = $expected', ({ x,y, expected}) => {
             let plateau: MARS.Plateau  = new MARS.Plateau(x,y);
-            expect(plateau).toEqual(expexted);
+            expect(plateau).toEqual(expected);
         });
 });
