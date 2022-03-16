db15522d3746122141607e9ce29b2bf4b0b31d1d
Cenk <muzzyathome@gmail.com>
2022-03-16 20:11:48 +0000

    Refactored invalid input test for plateau creation passing

--- index.ts
+++ index.ts
@@ -2,5 +2,8 @@ import {Plateau} from "./src/mars-plateau"
 
 export const createPlateau = (input: string):Plateau => {
     if (input === '') throw new Error("two numbers are required eg 5 5");
-    return new Plateau('5 5');
+    const size = input.split(' ').map(Number);
+    if (size.length !== 2) throw new Error("two numbers are required eg 5 5");
+    if ((Number.isNaN(size[0])) || (Number.isNaN(size[1]))) throw new Error("two numbers are required eg 5 5");
+    return new Plateau(input);
 } 
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,14 @@
 import { createPlateau } from "../index"
 // 1. Test the input for the plateau size x by y
 describe('Input needs to be two numbers seperated by a space character', () => {
-    
-    test("it throws an error if not passed a string of two numbers", () => {
-        expect(() => createPlateau("")).toThrow("two numbers are required eg 5 5");
-    });       
+    test.each`
+        input	
+        ${""}	
+        ${"S"}	
+        ${"5"}
+        ${"S 5"}
+        ${"5 S"}
+    `('createPlateau($input)', ({input}) => {
+        expect(() => createPlateau(input)).toThrow("two numbers are required eg 5 5");
+    });          
 });
