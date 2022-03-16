7d0bdc6455d02f3effbf41b84a1aba996801b0c7
Cenk <muzzyathome@gmail.com>
2022-03-16 19:26:34 +0000

    Test for accepting valid input for plateau creation

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+
+// 1. Test the input for the plateau size x by y
+describe('Input needs to be two numbers seperated by a space character', () => {
+    
+    test("it throws an error if not passed a string of two numbers", () => {
+        expect(() => createPlateau()).toThrow("two numbers are required eg 5 5");
+    });       
+});
